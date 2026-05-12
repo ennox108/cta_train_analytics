613ce49b83b591685cd00f8ba50b2a1420fe109f
Chinmay <chinmayjd09@gmail.com>
2026-05-12 18:43:28 +0000

    new branch

---
+++ logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-05-12T18:32:54.815257959+00:00
+-- finished_at: 2026-05-12T18:32:54.958522069+00:00
+-- elapsed: 143ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c452f8-0005-813d-0000-000dd5d6a161
+-- desc: not provided
+select 1 as id;
---
+++ target/.lsp/compiled/my_new_project/models/example/my_second_dbt_model.sql
@@ -0,0 +1,6 @@
+
+-- Use the `ref` function to select from other models
+
+select *
+from CTA_DB.dbt_cthakare.my_first_dbt_model
+where id = 1
